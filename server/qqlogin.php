7d8ce37939063a97c0fbbaf85342ce924b7e0c49
Jun <idche@qq.com>
2011-12-20 17:26:01 +0800

    update

---
+++ server/qqlogin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="zh">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Hello Jun</title>
+</head>
+<body>
+	<script>
+	
+		function callback(user){
+			console.log(user);
+			console.log(window.opener);
+		};
+		
+		if(window.location.hash.length > 1){
+			var accessToken = window.location.hash.substring(1);
+			var path = "https://graph.qq.com/oauth2.0/me?";
+			var queryParams = [accessToken, 'callback=callback'];
+			var query = queryParams.join('&');
+			var url = path + query;
+			var script = document.createElement('script');
+			script.src = url;
+			document.body.appendChild(script);
+		}
+	</script>
+</body>
+</html>
